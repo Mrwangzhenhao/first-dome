b782cac5b5a9f73146fe5bb5f508e31a9fb50307
王圳豪 <1153515745@qq.com>
2023-03-05 14:45:26 +0800

    修改main.c

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 include <reg51.h>
 
+void Delay_s(unsigned int t);
+
 void main(void)
 {
   while(1)
   {
     
-  };
-};
+  }
+}
+
+void Delay_s(unsigned int t)		//@11.0592MHz
+{
+	unsigned char i, j, k,x;
+	for(x=t;x>0;x--)
+	{
+		_nop_();
+		i = 8;
+		j = 1;
+		k = 243;
+		do
+		{
+			do
+			{
+				while (--k);
+			} while (--j);
+		} while (--i);
+	}	
+}
+
